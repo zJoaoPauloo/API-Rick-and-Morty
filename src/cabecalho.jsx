515ab473ebc207e3b2f2da5788da26c4962f9ec7
zJoaoPauloo <160547482+zJoaoPauloo@users.noreply.github.com>
2024-05-21 20:14:05 -0300

    ...
    ...

--- src/cabecalho.jsx
+++ src/cabecalho.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Link, Outlet} from 'react-router-dom';
-function Cabecalho(){
-return(
-   <>
-    <h1>lista de tarefas</h1>
-    <Link to={'/tarefas'}>Tarefas</Link>
-    <Link to={'rota2'}>rota2</Link>
-    <Link to={'/consulta-ram'}> Consulta rick morty</Link>
-    <Link to={'/detalhePersonagem'}>detalhes dos personagens</Link>
-    <Outlet/>
+import { Link, Outlet } from 'react-router-dom';
+
+function Cabecalho() {
+  return (
+    <>
+      <h1>lista de tarefas</h1>
+      <Link to={'/tarefas'}>Tarefas</Link>{' '}
+      <Link to={'rota2'}>rota2</Link>{' '}
+      <Link to={'/consulta-ram'}>Consulta rick morty</Link>{' '}
+      <Link to={'/detalhePersonagem'}>detalhes dos personagens</Link>
+      <Outlet />
     </>
-);
+  );
 }
+
 export default Cabecalho;
